4e4333c9e3c92d16b78be8b858c92e69291663d0
Benno <57860196+bennoinbeta@users.noreply.github.com>
2026-05-06 18:46:09 +0200

    #develop fix typo

--- apps/desktop/src/routes/window.main._sidebar/components/SidebarItem.tsx
+++ apps/desktop/src/routes/window.main._sidebar/components/SidebarItem.tsx
@@ -14,7 +14,8 @@ export const SidebarItem: React.FC<TSidebarItemProps> = (props) => {
 				className: cn(
 					'flex w-full items-center gap-2 rounded-lg px-2 py-1 text-left',
 					'text-base-600 text-sm transition-colors',
-					"hover:bg-base-950/6 hover:text-base-950 focus-ring border border-transparent select-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+					'hover:bg-base-950/6 hover:text-base-950 focus-ring border border-transparent select-none disabled:pointer-events-none disabled:opacity-50',
+					"[&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
 					!isAction && 'data-[status=active]:bg-base-950/10 data-[status=active]:text-base-950',
 					className
 				),
